298bb1abd920669bff920e6365ad144f5372feb7
Facundo Cordoba <facundo.cordoba@scanntech.com>
2026-04-27 15:08:03 -0300

    resolution module

--- msvc-reactive-web-flux/msvc_crud_products/src/main/java/com/crdev/msvc_crud_products/controller/ProductController.java
+++ msvc-reactive-web-flux/msvc_crud_products/src/main/java/com/crdev/msvc_crud_products/controller/ProductController.java
@@ -2,30 +2,36 @@ package com.crdev.msvc_crud_products.controller;
 
 import com.crdev.msvc_crud_products.domain.Product;
 import com.crdev.msvc_crud_products.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("api/v1")
+@RestController
+@RequestMapping("api/v1")
 public class ProductController {
 
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-     @GetMapping("/products")
-     public List<Product> getAllProducts() {
-         return productService.findAll();
-     }
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.findAll();
+    }
 
+    @GetMapping("/products/category/{category}")
+    public List<Product> getAllProductsByCategory(@PathVariable String category) {
+        return productService.findByCategory(category);
+    }
 
-     @GetMapping("/products/category/{category}")
-     public List<Product> getAllProductsByCategory(@PathVariable String category) {
-         return productService.findByCategory(category);
-     }
-
-     @GetMapping("/products")
-     public Product getProductByCode(@RequestParam ("code")int code) {
-         return productService.findByCode(code);
-     }
+    @GetMapping("/products/search")
+    public Product getProductByCode(@RequestParam("code") int code) {
+        return productService.findByCode(code);
+    }
 }
