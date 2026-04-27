5ea9120d960ce6cd4dd55eed03c71a0e67b20db8
Facundo cordoba <cordobafs@gmail.com>
2026-04-27 16:32:26 -0300

    firts endpoint

--- msvc-reactive-web-flux/msvc_crud_products/src/main/java/com/crdev/msvc_crud_products/controller/ProductController.java
+++ msvc-reactive-web-flux/msvc_crud_products/src/main/java/com/crdev/msvc_crud_products/controller/ProductController.java
@@ -2,16 +2,13 @@ package com.crdev.msvc_crud_products.controller;
 
 import com.crdev.msvc_crud_products.domain.Product;
 import com.crdev.msvc_crud_products.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -20,18 +17,34 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/products")
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAllProducts() {
         return productService.findAll();
     }
 
-    @GetMapping("/products/category/{category}")
-    public List<Product> getAllProductsByCategory(@PathVariable String category) {
+    @GetMapping(value = "/category/{category}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Product> getAllProductsByCategory(@PathVariable String category ) {
         return productService.findByCategory(category);
     }
 
-    @GetMapping("/products/search")
-    public Product getProductByCode(@RequestParam("code") int code) {
+    @GetMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE  )
+    public Product getProductByCode(@PathVariable("code") int code) {
         return productService.findByCode(code);
     }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void save(@RequestBody Product product ) {
+        productService.save(product);
+    }
+
+    @DeleteMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product delete(@PathVariable int code) {
+       return productService.delete(code);
+    }
+
+    @PatchMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product update(@PathVariable int code, @RequestBody Product product) {
+        return productService.updatePriceUnit(code, product.getPriceUnit());
+    }
+
 }
