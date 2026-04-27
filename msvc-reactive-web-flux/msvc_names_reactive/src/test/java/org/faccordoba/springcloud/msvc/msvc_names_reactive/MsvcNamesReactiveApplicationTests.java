a6026756244668fff9b5cde857914fc30ed89f35
Facundo cordoba <cordobafs@gmail.com>
2026-04-27 16:49:58 -0300

    add msvc names reactive

---
+++ msvc-reactive-web-flux/msvc_names_reactive/src/test/java/org/faccordoba/springcloud/msvc/msvc_names_reactive/MsvcNamesReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.faccordoba.springcloud.msvc.msvc_names_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcNamesReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
