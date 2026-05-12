312fcc1cb47c106dfe6bf80ceaf57d1bd0eed0ae
Facundo cordoba <cordobafs@gmail.com>
2026-05-11 22:54:09 -0300

    refactor ProductController to return ResponseEntity for all endpoints

--- msvc-reactive-web-flux/msvc_crud_productos_reactivo/src/main/java/org/faccordoba/springcloud/msvc/msvc_crud_productos_reactivo/controller/ProductController.java
+++ msvc-reactive-web-flux/msvc_crud_productos_reactivo/src/main/java/org/faccordoba/springcloud/msvc/msvc_crud_productos_reactivo/controller/ProductController.java
@@ -2,7 +2,9 @@ package org.faccordoba.springcloud.msvc.msvc_crud_productos_reactivo.controller;
 
 import org.faccordoba.springcloud.msvc.msvc_crud_productos_reactivo.domain.Product;
 import org.faccordoba.springcloud.msvc.msvc_crud_productos_reactivo.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -18,32 +20,36 @@ public class ProductController {
     }
 
     @GetMapping(value = "/")
-    public Flux<Product> findAll() {
-        return productService.findAll();
+    public ResponseEntity<Flux<Product>> findAll() {
+        return  ResponseEntity.ok(productService.findAll());
     }
 
     @GetMapping(value = "/category/{category}")
-    public Flux<Product> getAllProductsByCategory(@PathVariable String category ) {
-         return productService.findByCategory(category);
+    public ResponseEntity<Flux<Product>> getAllProductsByCategory(@PathVariable String category ) {
+         return  ResponseEntity.ok(productService.findByCategory(category));
      }
 
-    @GetMapping(value = "/{code}", produces = MediaType.TEXT_EVENT_STREAM_VALUE  )
-    public Mono<Product> getProductByCode(@PathVariable("code") int code) {
-        return productService.findByCode(code);
+    @GetMapping(value = "/{code}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<Mono<Product>> getProductByCode(@PathVariable("code") int code) {
+        return ResponseEntity.ok(productService.findByCode(code));
      }
      @PostMapping(value = "/", consumes = MediaType.TEXT_EVENT_STREAM_VALUE)
-     public Mono<Void> save(@RequestBody Product product ) {
-        return productService.save(product);
+     public ResponseEntity<Mono<Void>> save(@RequestBody Product product ) {
+        return  ResponseEntity.ok(productService.save(product));
     }
 
     @DeleteMapping(value = "/{code}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Mono<Product> delete(@PathVariable int code) {
-        return productService.delete(code);
+    public Mono<ResponseEntity<Product>> delete(@PathVariable int code) {
+         return productService.delete(code)
+                 .map(ResponseEntity::ok)
+                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
     @PatchMapping(value = "/{code}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Mono<Product> update(@PathVariable int code, @RequestBody Product product) {
-        return productService.updatePriceUnit(code, product.getPriceUnit());
+    public Mono<ResponseEntity<Product>> update(@PathVariable int code, @RequestBody Product product) {
+        return productService.updatePriceUnit(code, product.getPriceUnit())
+                .map(ResponseEntity::ok)
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
 }
