c7b3a6268a03879ce5e0dbcb1b3b82cabdc08396
Facundo Cordoba <facundo.cordoba@scanntech.com>
2026-07-31 12:22:57 -0300

    msvc- rastreador

---
+++ msvc-reactive-web-flux/test-crud-products-spring/src/main/java/com/crdev/testcrudproductsspring/TestCrudProductsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.crdev.testcrudproductsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudProductsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCrudProductsSpringApplication.class, args);
+    }
+
+}
